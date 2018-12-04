f96eb5a1d6e7aea7faa3fa038c36ba93f0f3c21c
BrichOussama <oussama.brich@alumni.univ-avignon.fr>
2018-12-04 02:14:16 +0100

    addElement function ...

--- test.py
+++ test.py
@@ -31,4 +31,30 @@ def FileCreator():
 						os.mkdir(path2+"/" + os.path.splitext(os.path.basename(n))[0])
 					
 # main or to test function Q					
+				
 					
+def Addelement():
+	''''''
+		list = os.listdir('.') 
+		path = os.path.dirname(os.path.abspath(__file__))
+		
+		for index in list :
+		
+			if index.endswith(".txt"):
+				'''read file with r perm'''
+				file= open (index ,"r")
+				
+				file_a = open (os.path.splitext(os.path.basename(index))[0]+'modif' , "w")
+				'''titre'''
+				file_a.write ("titre : " +os.path.splitext(os.path.basename(index))[0] +"\n")
+				'''writere add element'''
+			for i , line in enumerate(file):
+					''''''
+					if i == 3 :
+						file_a.write("l'auteur  : " + line)
+						file.close
+						# out.writelines
+						file_a.close
+		
+
+
