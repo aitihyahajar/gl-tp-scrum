094d1b566f48c7cd99950b52414daf5cc5b52e53
brich <oussama.brich@alumni.univ-avignon.fr>
2019-01-20 00:12:33 +0100

    Fix indice

--- convert1.bat
+++ convert1.bat
@@ -3,15 +3,31 @@ mkdir -p text/
 mkdir -p info/
 cd Papers
 
+
+
 echo 	 "Liste et indice des pdfs : "
-indice=0
+indice=1
 for i in *.pdf
 do
-	pdftotext -o "../text/$i.txt" "$i"
-	echo "----------------------------------------"
+	
+	if [ "$i" != "${i// /_}" ]
+	then
+		mv "$i" "${i// /_}"
+	fi
+
+	i="${i// /_}"
+		
+	fname="${i%.*}"
+	
+	echo $fname
+	echo "" >  ../text/$fname.txt
+	
+	pdftotext $i "../text/$fname.txt"
+
 	echo "$i indice=$indice"
 	echo 
-	((indice=indice+1))	
+	((indice=indice+1))
+	echo "----------------------------------------"	
 done
 echo "----------------------------------------"
 echo "pour convertir la forme de coverte(txt ou XML) "
